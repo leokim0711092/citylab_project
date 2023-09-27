a7d51da3c650f4bdf1e73ee4c2d6b97c1a506d20
leokim0711092 <leokim10073@gmail.com>
2023-09-27 07:40:49 +0000

    Add mutiple executor

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -20,9 +20,18 @@ class Turtle_bot_mv: public rclcpp::Node{
 
     public:
         Turtle_bot_mv(): Node("Move_turtlebot_node"){
+
+        
+            callback_group_1 = this->create_callback_group(
+                rclcpp::CallbackGroupType::MutuallyExclusive);
+            callback_group_2 = this->create_callback_group(
+                rclcpp::CallbackGroupType::MutuallyExclusive);
+            auto sub1_ = rclcpp::SubscriptionOptions();
+            sub1_.callback_group = callback_group_1;
+
             Pub = this ->create_publisher<geometry_msgs::msg::Twist>("cmd_vel",10);
-            Sub = this-> create_subscription<sensor_msgs::msg::LaserScan>("scan",10,std::bind(&Turtle_bot_mv::turtle_callback,this, std::placeholders::_1));    
-            timer = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&Turtle_bot_mv::timer_callback,this));
+            Sub = this-> create_subscription<sensor_msgs::msg::LaserScan>("scan",10,std::bind(&Turtle_bot_mv::turtle_callback,this, std::placeholders::_1),sub1_);    
+            timer = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&Turtle_bot_mv::timer_callback,this),callback_group_2);
         }
     private:
         void turtle_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
@@ -31,36 +40,59 @@ class Turtle_bot_mv: public rclcpp::Node{
             bool ck =true;
             bool rg_ck = true;
             for(size_t i=0; i< msg->ranges.size();i++){
-                if (msg->ranges[i] > 0.3 && rg_ck) {
-                    
-                    if(rc< msg->ranges[i] && msg->ranges[i] != INFINITY ){
-                        rc = msg->ranges[i];
-                        std::cout << i <<"1  :" << rc << std::endl;
-                        angle = i;
-                    }
 
-                }else if ( msg->ranges[i] <= 0.3 || !rg_ck) {
-                    rg_ck = false;
-                    if (angle<360 && i<360 && rc< msg->ranges[i] && msg->ranges[i] != INFINITY) {
-                        rc = msg->ranges[i];
-                        std::cout << i <<"2  :" << rc << std::endl;
-                        angle = i;
-                        ck = false;
-                    }else if (angle>359 && i>359 && ck && rc< msg->ranges[i] && msg->ranges[i] != INFINITY) {
-                        rc = msg->ranges[i];
-                        std::cout << i <<"3  :" << rc << std::endl;
-                        angle = i;
+                    if (msg->ranges[i] > 0.35 && rg_ck) {
+                        
+                        if(rc< msg->ranges[i] && msg->ranges[i] != INFINITY ){
+                            rc = msg->ranges[i];
+                            std::cout << i <<"1  :" << rc << std::endl;
+                            angle = i;
+                        }
+
+                    }else if ( msg->ranges[i] <= 0.35|| !rg_ck) {
+                        rg_ck = false;
+                        if (angle<360 && i<360 && rc< msg->ranges[i] && msg->ranges[i] != INFINITY) {
+                            rc = msg->ranges[i];
+                            std::cout << i <<"2  :" << rc << std::endl;
+                            angle = i;
+                            ck = false;
+                        }else if (angle>359 && i>359 && ck && rc< msg->ranges[i] && msg->ranges[i] != INFINITY) {
+                            rc = msg->ranges[i];
+                            std::cout << i <<"3  :" << rc << std::endl;
+                            angle = i;
+                        }
                     }
-                }
+                    // if (msg->ranges[719-i] > 0.25 && rg_ck) {
+                        
+                    //     if(rc< msg->ranges[719-i] && msg->ranges[719-i] != INFINITY ){
+                    //         rc = msg->ranges[719-i];
+                    //         std::cout << 719-i <<"1  :" << rc << std::endl;
+                    //         angle = 719-i;
+                    //     }
+
+                    // }else if ( msg->ranges[719-i] <= 0.25 || !rg_ck) {
+                    //     rg_ck = false;
+                    //     if (angle<360 && 719-i<360 && rc< msg->ranges[719-i] && msg->ranges[719-i] != INFINITY) {
+                    //         rc = msg->ranges[719-i];
+                    //         std::cout << 719-i <<"2  :" << rc << std::endl;
+                    //         angle = 719-i;
+                    //         ck = false;
+                    //     }else if (angle>359 && 719-i>359 && ck && rc< msg->ranges[719-i] && msg->ranges[719-i] != INFINITY) {
+                    //         rc = msg->ranges[719-i];
+                    //         std::cout << 719-i <<"3  :" << rc << std::endl;
+                    //         angle = 719-i;
+                    //     }
+                    // }
+                
                 // std::cout << i <<":" << msg->ranges[i] << std::endl;
             }
             direction_ = M_PI/2 - (angle/719.0)*M_PI;
-            RCLCPP_INFO(this->get_logger(),"%f",direction_);
             
         }
         void timer_callback(){
-            vel.linear.x = 0.05;
+            vel.linear.x = 0.1;
             vel.angular.z = direction_*0.5;
+            RCLCPP_INFO(this->get_logger(),"%f",vel.angular.z);
             Pub->publish(vel);
         }
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr Pub;
@@ -68,12 +100,16 @@ class Turtle_bot_mv: public rclcpp::Node{
         geometry_msgs::msg::Twist vel;
         rclcpp::TimerBase::SharedPtr timer;
         float rc = 0.0;
-        
+        rclcpp::CallbackGroup::SharedPtr callback_group_1;
+        rclcpp::CallbackGroup::SharedPtr callback_group_2;
         float direction_;
 };
 
 int main(int argc, char** argv){
-    rclcpp::init(argc,argv);
-    rclcpp::spin(std::make_shared<Turtle_bot_mv>());
+    rclcpp::init(argc,argv);  
+    std::shared_ptr<Turtle_bot_mv> node = std::make_shared<Turtle_bot_mv>();
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
     rclcpp::shutdown();
 }
