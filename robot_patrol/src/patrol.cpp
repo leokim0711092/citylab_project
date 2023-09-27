4b5c88a9396a790100fdd9e78518a44d821e5bf2
leokim0711092 <leokim10073@gmail.com>
2023-09-27 16:16:31 +0000

    New setup of turn

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -36,14 +36,14 @@ class Turtle_bot_mv: public rclcpp::Node{
     private:
         void turtle_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
             rc = msg->range_min;
-            size_t angle = 0;
+            int angle = 0;
             bool ck =true;
-            bool rg_ck = true;
-            bool middle_ck =true;
+            bool rg_ck = false;
+            std::vector<int> str;
+            // bool middle_ck =true;
             const float sf = 0.45;
-            for(size_t i=0; i< msg->ranges.size();i++){
-                    // std::cout << "msg  "<<i <<":" << msg->ranges[i] << std::endl;
-                    if (msg->ranges[i] > sf && rg_ck) {
+            for(int i= 719 ; i >= 0;i--){
+                    if (msg->ranges[i] > sf || rg_ck) {
                         
                         if(rc< msg->ranges[i] && msg->ranges[i] != INFINITY){
                             rc = msg->ranges[i];
@@ -51,35 +51,36 @@ class Turtle_bot_mv: public rclcpp::Node{
                             angle = i;
                         }
 
-                    }else if ( msg->ranges[i] <= sf || !rg_ck) {
+                    }else if ( msg->ranges[i] <= sf && !rg_ck) {
                         rg_ck = false;
-                        if (angle<360 && i<360 && rc< msg->ranges[i] && msg->ranges[i] != INFINITY) {
-                            rc = msg->ranges[i];
-                            std::cout << "2  "<<i <<":" << rc << std::endl;
-                            if(middle_ck) angle = i;
-                            else if (!middle_ck) angle = i -25;
-                    
-                            ck = false;
-                            if(i<360 && i> 340 && middle_ck){
-                                    int j=0;
-                                    while (j<180) {
-                                        if (msg->ranges[360+j] <= sf) {
-                                                middle_ck = false;
-                                                angle = i -25;
-                                                break;
-                                        }
-                                        j++;
-                                    }
-                            }
-                
-                        }else if (angle>359 && i>359 && i<720&& ck && rc< msg->ranges[i] && msg->ranges[i] != INFINITY) {
-                            rc = msg->ranges[i];
-                            std::cout << "3  "<<i <<":" << rc << std::endl;
-                            angle = i;
-                        }
+                        str.push_back(i);
                     }
             }
-            direction_ = M_PI/2 - (angle/719.0)*M_PI;
+            direction_ = -M_PI/2 + (angle/719.0)*M_PI;
+            if(!rg_ck){
+                int rcrd[720];
+                for(int j=0;j<720;j++) rcrd[j] =0;
+                for(int i=0;i<str.size();i++){
+                    rcrd[str[i]] = 1;
+                }
+               std::vector<int> len_number_store[10]; //record the number of each start and end
+               int record_len = 0; // record the length
+               int record_hole_amount =0;
+               // know the direction
+               for(int j=0;j<720;j++){
+
+                    if(rcrd[j] == 1 && record_len ==0){
+                        len_number_store[record_hole_amount].push_back(j);
+                        record_len++;
+                    }else if (rcrd[j] ==1 ) record_len++; 
+                    else if (rcrd[j] ==0 && record_len > 0) {
+                        len_number_store[record_hole_amount].push_back(j-1);
+                        record_hole_amount++;
+                        record_len = 0;
+                    }
+               }
+                angle = Turtle_bot_mv::direction(len_number_store)*2;
+            }
             
         }
         void timer_callback(){
@@ -87,6 +88,22 @@ class Turtle_bot_mv: public rclcpp::Node{
             vel.angular.z = direction_*0.5;
             RCLCPP_INFO(this->get_logger(),"%f",vel.angular.z);
             Pub->publish(vel);
+        }
+        int direction(std::vector<int> len_store[10]){
+            int sz=0;
+            int hole_size=0;
+            int ang = 0;
+            int vec_number =0;
+                while(!len_number_store[++sz].empty()){
+
+                    hole_size = len_number_store[sz-1][1] -len_number_store[sz-1][0];
+                    if(hole_size < len_number_store[sz][1] -len_number_store[sz][0]){
+                        hole_size = len_number_store[sz][1] -len_number_store[sz][0];
+                        vec_number = sz;
+                    }
+                    ang = (len_number_store[vec_number][1] + len_number_store[vec_number][0])/2
+                }
+            return ang;
         }
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr Pub;
         rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr Sub;
